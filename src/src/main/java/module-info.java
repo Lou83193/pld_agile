0c9b7e244f6958f0c7b711cebcaf7033678a2272
Marcus Toma <best4tdm@gmail.com>
2021-11-17 08:46:40 +0100

    initial commit

---
+++ src/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.pld.agile {
+    requires javafx.controls;
+    requires javafx.fxml;
+    exports com.pld.agile.view;
+    //exports com.pld.agile.model;
+    //exports com.pld.agile.controller;
+}
