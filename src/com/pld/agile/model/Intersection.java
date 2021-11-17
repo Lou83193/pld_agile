9da64562c2d4acc72d368bf2fd37a11a44e6783f
Lou83193 <lou.bezzina@gmail.com>
2021-11-17 11:17:49 +0100

    getters, setters, constructor for intersection and segment

--- src/com/pld/agile/model/Intersection.java
+++ src/com/pld/agile/model/Intersection.java
@@ -15,4 +15,37 @@ public class Intersection {
      * Geographical longitude of the intersection.
      */
     private double longitude;
+
+    // CONSTRUCTOR
+    public Intersection(int id, int latitude, int longitude) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    // GETTERS
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // SETTERS
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
--- src/com/pld/agile/model/MapData.java
+++ src/com/pld/agile/model/MapData.java
@@ -6,10 +6,12 @@ public class MapData {
     /**
      * Map of all intersections, key is the Intersection ID.
      */
+    //todo: key is lat long
     HashMap <Integer, Intersection> IntersectionList = new HashMap();
 
     /**
      * Map of all segments, key is ...
      */
+    //todo: list
     HashMap <Integer, Segment> SegmentList = new HashMap();
 }
