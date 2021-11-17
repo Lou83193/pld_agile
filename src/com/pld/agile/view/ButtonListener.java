14d355f8949c136099ebd774b4745db09479e2a0
Marcus Toma <best4tdm@gmail.com>
2021-11-17 11:51:02 +0100

    generic button listener

---
+++ src/com/pld/agile/view/ButtonListener.java
@@ -0,0 +1,30 @@
+package com.pld.agile.view;
+
+import com.pld.agile.controller.Controller;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class ButtonListener implements EventHandler<ActionEvent> {
+
+    private Controller controller;
+    private String buttonType; //TODO: make enum
+
+    public ButtonListener(Controller controller, String buttonType) {
+        this.controller = controller;
+        this.buttonType = buttonType;
+    }
+
+    public void handle(ActionEvent e) {
+
+        switch(buttonType) {
+
+            case "loadMap" :
+                //controller.loadMap(path);
+                break;
+
+        }
+
+        e.consume();
+    }
+
+}
