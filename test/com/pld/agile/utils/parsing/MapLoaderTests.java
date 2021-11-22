f1177fa6891eefa53a990d8f613533c1bcdb37a7
emelink <emelink@example.com>
2021-11-22 11:12:38 +0100

    Implemented first test of UseCase 1

---
+++ test/com/pld/agile/utils/parsing/MapLoaderTests.java
@@ -0,0 +1,31 @@
+package com.pld.agile.utils.parsing;
+
+import com.pld.agile.model.map.MapData;
+import com.pld.agile.utils.parsing.MapLoader;
+
+import org.dom4j.DocumentException;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MapLoaderTests {
+
+    @Test
+    public void testLoadFileNoMapNode() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        //redirect the System-output (normaly the console) to a variable
+        System.setErr(new PrintStream(outContent));
+
+        String filePath = "test/resources/loadMap_noMapNode";
+        MapData mapData = MapData.getInstance();
+        MapLoader mapLoader = new MapLoader(filePath, mapData);
+        boolean res = mapLoader.load();
+        assertFalse(res);
+        //check if your error message is in the output variable
+        assertEquals("org.dom4j.DocumentException: Error on line 3 of document", outContent.toString().substring(0, 56));
+    }
+
+}
