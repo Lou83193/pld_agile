5a87fa5695942b0c6a7c7a544f66af685bc23cc2
emelink <emelink@example.com>
2021-11-22 11:36:46 +0100

    Edited some Tests

--- test/com/pld/agile/model/tour/TourDataTest.java
+++ test/com/pld/agile/model/tour/TourDataTest.java
@@ -1,19 +1,15 @@
-package com.pld.agile;
+package com.pld.agile.model.tour;
 
 import com.pld.agile.model.map.MapData;
-import com.pld.agile.utils.parsing.MapLoader;
-import com.pld.agile.model.tour.TourData;
 import com.pld.agile.utils.parsing.RequestLoader;
 import org.junit.jupiter.api.Test;
 
 class TourDataTest {
-    private String filePath = "test/resources/loadMap_loadRequestsBase";
-    private MapData mapData = new MapData ();
-    private MapLoader mapLoader = new MapLoader(filePath, mapData);
-    private boolean success = mapLoader.load();
-    private TourData tourDataOriginal = new TourData();
-    private RequestLoader requestLoader = new RequestLoader("test/resources/loadRequests_3Requests", tourDataOriginal);
-    private TourData tourData = new TourData();
+    private final MapData mapData = new MapData ();
+    private boolean success;
+    private final TourData tourDataOriginal = new TourData();
+    private final RequestLoader requestLoader = new RequestLoader("test/resources/loadRequests_3Requests", tourDataOriginal);
+    private TourData tourData;
 
     @Test
     //Test n°3.1
