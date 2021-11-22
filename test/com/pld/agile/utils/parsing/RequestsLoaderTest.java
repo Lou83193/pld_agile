4e635c35d16b675a43668eab50db808da1d9bc8a
Dodol2020 <62092801+Dodol2020@users.noreply.github.com>
2021-11-22 09:40:14 +0100

    Tests pour loadRequest

---
+++ test/com/pld/agile/utils/parsing/RequestsLoaderTest.java
@@ -0,0 +1,73 @@
+package com.pld.agile.utils.parsing;
+
+import com.pld.agile.model.map.MapData;
+import com.pld.agile.model.tour.TourData;
+import org.junit.jupiter.api.Test;
+
+class RequestsLoaderTest {
+    private String filePath = "test/resources/loadMap_loadRequestsBase";
+    private MapData mapData = MapData.getInstance();
+    private MapLoader mapLoader = new MapLoader(filePath, mapData);
+    private boolean success = mapLoader.load();
+    @Test
+
+    //Test n°2.1
+    public void testLoadRequests_3Requests() {
+        TourData tourData = TourData.getInstance();
+        tourData.setAssociatedMap(mapData);
+        RequestsLoader requestsLoader = new RequestsLoader("test/resources/loadRequests_3Requests", tourData);
+        success = requestsLoader.load();
+
+        if(success) {
+            for (int i = 0; i < tourData.getRequestList().size();i++) {
+                if (tourData.getRequestList().get(i).getPickup().getAddress() != null && tourData.getRequestList().get(i).getDelivery().getAddress() != null){
+                    System.out.println("SUCCESS : " + success);
+                }else {
+                    System.out.println("FAIL : " + success);
+                }
+            }
+        }
+        System.out.println(tourData);
+    }
+
+    @Test
+    //Test n°2.2
+    public void testLoadRequests_redundancy() {
+        TourData tourData = TourData.getInstance();
+        tourData.setAssociatedMap(mapData);
+        RequestsLoader requestsLoader = new RequestsLoader("test/resources/loadRequests_redundancy.xml", tourData);
+        success = requestsLoader.load();
+
+        if(success) {
+            for (int i = 0; i < tourData.getRequestList().size();i++) {
+                if (tourData.getRequestList().get(i).getPickup().getAddress() != null && tourData.getRequestList().get(i).getDelivery().getAddress() != null){
+                    System.out.println("SUCCESS : " + success);
+                }else {
+                    System.out.println("FAIL : " + success);
+                }
+            }
+        }
+        System.out.println(tourData);
+    }
+
+    @Test
+    //Test n°2.3
+    public void testLoadRequests_depotAddressMissing() {
+        TourData tourData = TourData.getInstance();
+        tourData.setAssociatedMap(mapData);
+        RequestsLoader requestsLoader = new RequestsLoader("test/resources/loadRequests_depotAddressMissing.xml", tourData);
+        success = requestsLoader.load();
+
+        if(success) {
+            for (int i = 0; i < tourData.getRequestList().size();i++) {
+                if (tourData.getRequestList().get(i).getPickup().getAddress() != null && tourData.getRequestList().get(i).getDelivery().getAddress() != null){
+                    System.out.println("SUCCESS : " + success);
+                }else {
+                    System.out.println("FAIL : " + success);
+                }
+            }
+        }
+        System.out.println(tourData);
+    }
+
+}
