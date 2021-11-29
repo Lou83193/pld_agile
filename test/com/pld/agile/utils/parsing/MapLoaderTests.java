dbff9126315bf0a3331114b84d7f9c6e802278dc
emelink <emelink@example.com>
2021-11-29 11:50:04 +0100

    Implemented tests 1.5 and 1.6

--- test/com/pld/agile/utils/parsing/MapLoaderTests.java
+++ test/com/pld/agile/utils/parsing/MapLoaderTests.java
@@ -3,10 +3,10 @@ package com.pld.agile.utils.parsing;
 import com.pld.agile.model.map.Intersection;
 import com.pld.agile.model.map.MapData;
 import com.pld.agile.model.map.Segment;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,21 +16,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MapLoaderTests {
 
-    private MapData createMapData(List<Map<String, Object>> intersectionsData, List<Map<String, Object>> segmentsData) {
-        List<Intersection> intersections = new ArrayList<>();
-        for (Map<String, Object> item : intersectionsData) {
-            intersections.add(new Intersection((int) item.get("id"), (double) item.get("latitude"), (double) item.get("longitude")));
-        }
-        List<Segment> segments = new ArrayList<>();
-        for (Map<String, Object> item : segmentsData) {
-            segments.add(new Segment((String) item.get("name"), (int) item.get("length"), intersections.get((int) item.get("originId")), intersections.get((int) item.get("destinationId"))));
-        }
-
-        return new MapData(intersections, segments);
-    }
-
     @Test
-    // Test n°1.1
+    // Test nb 1.1
     public void test5Intersections4Segments() {
         String filePath = "test/resources/loadMap_5Inter4Seg.xml";
         MapData actualMapData = new MapData();
@@ -52,28 +39,139 @@ public class MapLoaderTests {
                 Map.ofEntries(Map.entry("name", "Rue de la Meuse"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 3)),
                 Map.ofEntries(Map.entry("name", "Rue de la Moselle"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 4))
         );
-
         MapData expectedMapData = createMapData(intersectionsData, segmentsData);
 
-        String expected = expectedMapData.toString();
-        assertEquals(expected, actualMapData.toString());
+        assertEquals(expectedMapData.toString(), actualMapData.toString());
     }
 
 
+    @Disabled("Disabled until removal of disconnected intersections from model on parsing is implemented")
     @Test
-    //Test n°1.4
-    public void testNoMapNode() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        //redirect the System-output (normally the console) to a variable
-        System.setErr(new PrintStream(outContent));
-
-        String filePath = "test/resources/loadMap_noMapNode.xml";
-        MapData mapData = new MapData();
-        MapLoader mapLoader = new MapLoader(filePath, mapData);
+    // Test nb 1.2
+    public void test4Intersection4Segments() {
+        String filePath = "test/resources/loadMap_4Inter4Seg.xml";
+        MapData actualMapData = new MapData();
+        MapLoader mapLoader = new MapLoader(filePath, actualMapData);
         boolean res = mapLoader.load();
-        assertFalse(res);
-        //check if the beginning of the error message is the one expected
-        assertEquals("org.dom4j.DocumentException: Error on line 3 of document", outContent.toString().substring(0, 56));
+        assertTrue(res);
+
+        List<Map<String, Object>> intersectionsData = Arrays.asList(
+                Map.ofEntries(Map.entry("id", 0), Map.entry("latitude", 45.100), Map.entry("longitude", 4.001)),
+                Map.ofEntries(Map.entry("id", 1), Map.entry("latitude", 45.101), Map.entry("longitude", 4.002)),
+                Map.ofEntries(Map.entry("id", 2), Map.entry("latitude", 45.102), Map.entry("longitude", 4.003)),
+                Map.ofEntries(Map.entry("id", 3), Map.entry("latitude", 45.103), Map.entry("longitude", 4.004)),
+                Map.ofEntries(Map.entry("id", 4), Map.entry("latitude", 45.104), Map.entry("longitude", 4.005))
+        );
+
+        List<Map<String, Object>> segmentsData = Arrays.asList(
+                Map.ofEntries(Map.entry("name", "Avenue Général Frère"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 1)),
+                Map.ofEntries(Map.entry("name", "Boulevard Général Frère"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 2)),
+                Map.ofEntries(Map.entry("name", "Rue de la Meuse"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 3)),
+                Map.ofEntries(Map.entry("name", "Rue de la Moselle"), Map.entry("length", 100), Map.entry("originId", 2), Map.entry("destinationId", 1))
+        );
+        MapData expectedMapData = createMapData(intersectionsData, segmentsData);
+
+        assertEquals(expectedMapData.toString(), actualMapData.toString());
+        assertEquals(4, actualMapData.getIntersections().size());
     }
 
+
+    @Disabled("Disabled until exception throw when no map tag is present is implemented")
+    @Test
+    // Test nb 1.3
+    public void testNoMapNode() {
+        String filePath = "test/resources/loadMap_noMapNode.xml";
+        MapData actualMapData = new MapData();
+        MapLoader mapLoader = new MapLoader(filePath, actualMapData);
+
+//        assertThrows(MapSyntaxException.class, mapLoader::load); // TODO: create MapSyntaxException
+    }
+
+
+    @Disabled("Disabled until IO exception throw when file inaccessible is implemented")
+    @Test
+    // Test nb 1.4
+    public void testIOException() {
+        String filePath = "test/resources/FILE_DOES_NOT_EXIST.xml";
+        MapData actualMapData = new MapData();
+        MapLoader mapLoader = new MapLoader(filePath, actualMapData);
+
+        assertThrows(IOException.class, mapLoader::load);
+    }
+
+
+    @Disabled("Disabled until removal of segment with same origin and destination is implemented")
+    @Test
+    // Test nb 1.5
+    public void testSameIntersectionSegment() {
+        String filePath = "test/resources/loadMap_sameIntersectionsSegment.xml";
+        MapData actualMapData = new MapData();
+        MapLoader mapLoader = new MapLoader(filePath, actualMapData);
+        boolean res = mapLoader.load();
+        assertTrue(res);
+
+        List<Map<String, Object>> intersectionsData = Arrays.asList(
+                Map.ofEntries(Map.entry("id", 0), Map.entry("latitude", 45.100), Map.entry("longitude", 4.001)),
+                Map.ofEntries(Map.entry("id", 1), Map.entry("latitude", 45.101), Map.entry("longitude", 4.002)),
+                Map.ofEntries(Map.entry("id", 2), Map.entry("latitude", 45.102), Map.entry("longitude", 4.003)),
+                Map.ofEntries(Map.entry("id", 3), Map.entry("latitude", 45.103), Map.entry("longitude", 4.004)),
+                Map.ofEntries(Map.entry("id", 4), Map.entry("latitude", 45.104), Map.entry("longitude", 4.005))
+        );
+
+        List<Map<String, Object>> segmentsData = Arrays.asList(
+                Map.ofEntries(Map.entry("name", "Avenue Général Frère"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 1)),
+                Map.ofEntries(Map.entry("name", "Boulevard Général Frère"), Map.entry("length", 100), Map.entry("originId", 2), Map.entry("destinationId", 2)),
+                Map.ofEntries(Map.entry("name", "Rue de la Meuse"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 3)),
+                Map.ofEntries(Map.entry("name", "Rue de la Moselle"), Map.entry("length", 100), Map.entry("originId", 2), Map.entry("destinationId", 1))
+        );
+        MapData expectedMapData = createMapData(intersectionsData, segmentsData);
+
+        assertEquals(expectedMapData.toString(), actualMapData.toString());
+        assertEquals(3, actualMapData.getSegments().size());
+    }
+
+
+    @Disabled("Disabled until removal of segment with length zero is implemented")
+    @Test
+    // Test nb 1.6
+    public void testSegmentLengthZero() {
+        String filePath = "test/resources/loadMap_segmentLength0.xml";
+        MapData actualMapData = new MapData();
+        MapLoader mapLoader = new MapLoader(filePath, actualMapData);
+        boolean res = mapLoader.load();
+        assertTrue(res);
+
+        List<Map<String, Object>> intersectionsData = Arrays.asList(
+                Map.ofEntries(Map.entry("id", 0), Map.entry("latitude", 45.100), Map.entry("longitude", 4.001)),
+                Map.ofEntries(Map.entry("id", 1), Map.entry("latitude", 45.101), Map.entry("longitude", 4.002)),
+                Map.ofEntries(Map.entry("id", 2), Map.entry("latitude", 45.102), Map.entry("longitude", 4.003)),
+                Map.ofEntries(Map.entry("id", 3), Map.entry("latitude", 45.103), Map.entry("longitude", 4.004)),
+                Map.ofEntries(Map.entry("id", 4), Map.entry("latitude", 45.104), Map.entry("longitude", 4.005))
+        );
+
+        List<Map<String, Object>> segmentsData = Arrays.asList(
+                Map.ofEntries(Map.entry("name", "Avenue Général Frère"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 1)),
+                Map.ofEntries(Map.entry("name", "Boulevard Général Frère"), Map.entry("length", 0), Map.entry("originId", 0), Map.entry("destinationId", 2)),
+                Map.ofEntries(Map.entry("name", "Rue de la Meuse"), Map.entry("length", 100), Map.entry("originId", 0), Map.entry("destinationId", 3)),
+                Map.ofEntries(Map.entry("name", "Rue de la Moselle"), Map.entry("length", 100), Map.entry("originId", 3), Map.entry("destinationId", 4))
+        );
+        MapData expectedMapData = createMapData(intersectionsData, segmentsData);
+
+        assertEquals(expectedMapData.toString(), actualMapData.toString());
+        assertEquals(3, actualMapData.getSegments().size());
+    }
+
+
+    private MapData createMapData(List<Map<String, Object>> intersectionsData, List<Map<String, Object>> segmentsData) {
+        List<Intersection> intersections = new ArrayList<>();
+        for (Map<String, Object> item : intersectionsData) {
+            intersections.add(new Intersection((int) item.get("id"), (double) item.get("latitude"), (double) item.get("longitude")));
+        }
+        List<Segment> segments = new ArrayList<>();
+        for (Map<String, Object> item : segmentsData) {
+            segments.add(new Segment((String) item.get("name"), (int) item.get("length"), intersections.get((int) item.get("originId")), intersections.get((int) item.get("destinationId"))));
+        }
+
+        return new MapData(intersections, segments);
+    }
 }
