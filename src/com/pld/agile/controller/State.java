7406b78bb16631232db0f204c1444fe5b1e987d2
dblasko <daniel.blasko.dev@gmail.com>
2021-12-01 09:11:01 +0100

    Map parsing resilienc

--- src/com/pld/agile/controller/State.java
+++ src/com/pld/agile/controller/State.java
@@ -54,12 +54,14 @@ public interface State {
 
                 return true;
             } catch (IOException e) {
+                e.printStackTrace();
                 Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK);
                 alert.setTitle("Error"); // force english
                 alert.setHeaderText("Map loading error");
                 alert.showAndWait();
                 return false;
             } catch (SyntaxException e) {
+                e.printStackTrace();
                 Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK);
                 alert.setTitle("Error"); // force english
                 alert.setHeaderText("Map loading error");
