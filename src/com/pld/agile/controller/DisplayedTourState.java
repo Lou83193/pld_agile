9a53313aa91c804aed9bb60328405ec8ebc7dcbd
Lou Bezzina <lou.bezzina@gmail.com>
2021-12-01 10:25:40 +0100

    doDeleteRequest in DisplayedTourState

--- src/com/pld/agile/controller/DisplayedTourState.java
+++ src/com/pld/agile/controller/DisplayedTourState.java
@@ -1,15 +1,20 @@
 package com.pld.agile.controller;
 
+import com.pld.agile.model.tour.Path;
 import com.pld.agile.model.tour.Request;
 import com.pld.agile.model.tour.Stop;
 import com.pld.agile.model.tour.TourData;
 import com.pld.agile.utils.parsing.RequestLoader;
+import com.pld.agile.utils.tsp.Graph;
 import com.pld.agile.view.ButtonEventType;
 import com.pld.agile.view.ButtonListener;
 import com.pld.agile.view.Window;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * State when the map and a list of requests are loaded.
@@ -77,11 +82,60 @@ public class DisplayedTourState implements State {
 
     @Override
     public void doDeleteRequest(Controller c, Window window, Request request) {
-        // look in tour data's list of paths to find the two stops of the request
-        // for both the pickup and the delivery:
-        // once the two corresponding paths have been found, fetch the other ends of the paths
-        // use tourdata's graph to get the path between them
-        // insert that path in tour data's tourPaths instead of the two previous paths
+        Stop pickup = request.getPickup();
+        Stop delivery = request.getDelivery();
+        Stop currentOrigin = null;
+        Stop currentDestination = null;
+
+        List<Path> tourPath = window.getTourData().getTourPaths();
+
+        for (int i = 0; i<tourPath.size(); i++) {
+            Path path = tourPath.get(i);
+
+            /** If we found the request which we want to remove in the previous iteration,
+             * we find the new path between the previous stop and the next stop,
+             * and we add it to the tourPath.
+             */
+            if (currentOrigin != null) {
+
+                //Store destination and remove path to it
+                currentDestination = path.getDestination();
+                tourPath.remove(path);
+
+                //Find new path
+                Graph stopsGraph = window.getTourData().getStopsGraph();
+                List<Integer> stops = window.getTourData().getStops();
+                int indexOrigin = -1, indexDestination = -1;
+                for (int j = 0; j < stops.size(); j++) {
+                    if(stops.get(j) == currentOrigin.getAddress().getId()){
+                        indexOrigin = j;
+                    } else if (stops.get(j) == currentDestination.getAddress().getId()){
+                        indexDestination = j;
+                    }
+                    if(indexOrigin != -1 && indexDestination != -1) { break; }
+                }
+
+                Path newPath = stopsGraph.getPath(indexOrigin, indexDestination);
+
+                //Insert in position i
+                tourPath.add(i, newPath);
+                currentOrigin = null;
+            }
+
+            /**
+             * If the destination of the current path
+             * is the stop which we want to remove
+             * we store the origin of that stop and
+             * remove the path to it
+             */
+            if (path.getDestination() == pickup || path.getDestination() == delivery) {
+                currentOrigin = path.getOrigin();
+                tourPath.remove(path);
+                i--;
+            }
+        }
+
+        window.getTourData().setStopTimeAndNumber();
     }
 
     @Override
