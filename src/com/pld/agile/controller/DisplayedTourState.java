7032937d7eccc0d34a28d208e4e9c92020f23261
Lou Bezzina <lou.bezzina@gmail.com>
2021-12-02 13:58:12 +0100

    doShiftStopOrderUp

--- src/com/pld/agile/controller/DisplayedTourState.java
+++ src/com/pld/agile/controller/DisplayedTourState.java
@@ -16,6 +16,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static com.pld.agile.model.tour.StopType.DELIVERY;
+import static com.pld.agile.model.tour.StopType.PICKUP;
+
 /**
  * State when the map and a list of requests are loaded.
  * User can load another map, load another list of requests or ask the app to compute the tour.
@@ -140,11 +143,71 @@ public class DisplayedTourState implements State {
 
     @Override
     public boolean doShiftStopOrderUp(Controller c, Window window, Stop stop) {
-        // here it might be easier to construct a list of stops from the tour (using tourPaths, the list of paths)
-        // then shift the stop up or down
-        // then reconstruct tourPaths by iterating through the modified order list and fetching the paths in the graph
-        // (otherwise you could also directly manipulate the list of paths but it might be a tricky algorithm)
-        // don't forget to change order attribute in stop
+        ArrayList<Stop> listStops = new ArrayList<>();
+        List<Path> tourPath = window.getTourData().getTourPaths();
+
+        listStops.add(tourPath.get(0).getOrigin());
+        int indexStop = 0;
+
+        //Build a list of all the stops in the tour in order
+        for(int i = 0; i < tourPath.size(); i++) {
+            Stop currStop = tourPath.get(i).getDestination();
+            listStops.add(currStop);
+            if (currStop == stop) { indexStop = i; }
+        }
+
+        //Check if the stop is allowed to move up
+        boolean canMove = true;
+        Stop stopAbove = null;
+        if (indexStop < 1) {
+            canMove = false;
+        } else {
+            stopAbove = listStops.get(indexStop - 2);
+            if (stop.getType() == DELIVERY) {
+                System.out.println("delivery");
+                //todo : check if delivery above pickup
+                if (stop.getRequest().equals(stopAbove.getRequest())) { System.out.println("cannot move"); canMove = false; }
+            }
+        }
+        System.out.println("check done :" + canMove);
+
+        if (canMove) {
+            //Shift the stop up one place
+            listStops.add(indexStop, stopAbove);
+            listStops.add(indexStop - 1, stop);
+            System.out.println("stop moved");
+
+            //Reconstruct tourData
+            for (int i = 0; i < listStops.size() - 1; i++) {
+                //Update stopNumber
+                listStops.get(i).setStopNumber(i);
+
+                //Get index of stops
+
+                List<Integer> stops = window.getTourData().getStops();
+                int indexOrigin = -1, indexDestination = -1;
+                for (int j = 0; j < stops.size(); j++) {
+                    if (stops.get(j) == listStops.get(i).getAddress().getId()) {
+                        indexOrigin = j;
+                    } else if (stops.get(j) == listStops.get(i + 1).getAddress().getId()) {
+                        indexDestination = j;
+                    }
+                    if (indexOrigin != -1 && indexDestination != -1) {
+                        break;
+                    }
+                }
+
+                //Add path to tourPath
+                Graph stopsGraph = window.getTourData().getStopsGraph();
+                tourPath.clear();
+                tourPath.add(stopsGraph.getPath(indexOrigin, indexDestination));
+                System.out.println("path added");
+            }
+
+            System.out.println("DONE");
+            return true;
+        }
+
         return false;
     }
 
