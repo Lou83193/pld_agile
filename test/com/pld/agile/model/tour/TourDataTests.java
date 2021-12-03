cd674fa43d7c96523a961e91fa51296918b7fd93
Dodol2020 <62092801+Dodol2020@users.noreply.github.com>
2021-12-03 22:46:57 +0100

    Tests tests tests

--- test/com/pld/agile/model/tour/TourDataTests.java
+++ test/com/pld/agile/model/tour/TourDataTests.java
@@ -1,3 +1,9 @@
+/*
+ * TourDataTests
+ *
+ * Copyright (c) 2021. Hexanomnom
+ */
+
 package com.pld.agile.model.tour;
 
 import com.pld.agile.model.map.MapData;
@@ -13,15 +19,14 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TourDataTests {
     private final MapData mapData = new MapData ();
     private final MapLoader mapLoader = new MapLoader("test/resources/loadMap_loadRequestsBase.xml", mapData);
     private TourData tourDataInit = new TourData();
     private RequestLoader requestLoader;
-    private TourData tourData = new TourData();;
+    private TourData tourData = new TourData();
 
     @BeforeEach
     public void loadMap (){
@@ -39,13 +44,48 @@ public class TourDataTests {
         TourData tourData = new TourData();
         assertEquals(new ArrayList<>(),tourData.getRequestList());
         assertEquals(new ArrayList<>(),tourData.getTourPaths());
-        assertEquals(null,tourData.getAssociatedMap());
-        assertEquals(null,tourData.getDepartureTime());
-        assertEquals(null,tourData.getWarehouse());
+        assertNull(tourData.getAssociatedMap());
+        assertNull(tourData.getDepartureTime());
+        assertNull(tourData.getWarehouse());
     }
 
     @Test
     public void testUnHighlightStop(){
+        requestLoader = new RequestLoader("test/resources/computeTour_notOptimalTour.xml", tourData);
+        try {
+            requestLoader.load();
+        } catch (SyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (Request r:tourData.getRequestList()) {
+            r.getPickup().setHighlighted(1);
+            r.getDelivery().setHighlighted(1);
+        }
+        tourData.unHighlightStops();
+        int sum = 0;
+        for (Request r:tourData.getRequestList()) {
+            sum += r.getPickup().getHighlighted();
+            sum += r.getDelivery().getHighlighted();
+        }
+        assertEquals(0,sum);
+    }
+
+    @Test
+    public void testDeleteRequest (){
+        requestLoader = new RequestLoader("test/resources/computeTour_notOptimalTour.xml", tourData);
+        try {
+            requestLoader.load();
+        } catch (SyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        tourData.deleteRequest(tourData.getRequestList().get(1));
+
+        assertThrows(IndexOutOfBoundsException.class,()-> tourData.getRequestList().get(1));
 
     }
     @Test
@@ -63,8 +103,7 @@ public class TourDataTests {
         tourData = tourDataInit;
         tourData.computeTour();
 
-        assertFalse(tourData.getStops().toString().equals(tourDataInit.getStops().toString()));
-
+        assertNotEquals(tourData.getStops().toString(), tourDataInit.getStops().toString());
     }
 
 
