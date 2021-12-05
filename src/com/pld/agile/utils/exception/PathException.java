ac8d464d9b27808abf651e2083e5d3c6c4f8faf0
dblasko <daniel.blasko.dev@gmail.com>
2021-12-05 12:12:47 +0100

    Add Javadoc

--- src/com/pld/agile/utils/exception/PathException.java
+++ src/com/pld/agile/utils/exception/PathException.java
@@ -6,6 +6,9 @@
 
 package com.pld.agile.utils.exception;
 
+/**
+ * Exception raised when there is an error computing a path.
+ */
 public class PathException extends Exception {
     public PathException(String errMsg) {
         super(errMsg);
--- src/com/pld/agile/utils/exception/SyntaxException.java
+++ src/com/pld/agile/utils/exception/SyntaxException.java
@@ -6,6 +6,9 @@
 
 package com.pld.agile.utils.exception;
 
+/**
+ * Exception raised when a loaded XML file is of incoherent syntax that doesn't make it possible to load a tour / map.
+ */
 public class SyntaxException extends Exception {
     public SyntaxException(String errMsg) {
         super(errMsg);
